76e66d491a43a4e35dd429a712b6770e74ef37f1
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-09-16 19:47:51 +0100

    Removed unused variables

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
 const http = require('http');
-const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 
 var app = Express();
@@ -13,13 +12,11 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
-var database, collection;
 const password = "mahmoud808";
 
 const cityRouter = require('./cityRouter');
 
 const uri = "mongodb+srv://Cogi:" + password + "@city-data-db-3bqg0.mongodb.net/test?retryWrites=true&w=majority";
-const DATABASE_NAME = "city-data-db";
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true });
 
