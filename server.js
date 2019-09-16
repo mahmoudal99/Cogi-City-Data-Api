206ae3530f22435f745cff54bec322e73f0a15f3
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-09-16 19:49:45 +0100

    Code clean up

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const http = require('http');
 const mongoose = require('mongoose');
-
+const cityRouter = require('./cityRouter');
 var app = Express();
 
 app.use(BodyParser.json());
@@ -10,16 +10,13 @@ app.use(BodyParser.urlencoded({extended: true}));
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-
 server.listen(port);
+
 const password = "mahmoud808";
 
-const cityRouter = require('./cityRouter');
 
 const uri = "mongodb+srv://Cogi:" + password + "@city-data-db-3bqg0.mongodb.net/test?retryWrites=true&w=majority";
-
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true });
-
 app.use('/cities', cityRouter);
 
 
